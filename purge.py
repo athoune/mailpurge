23eef3d64117f0bebdd60d04fff539a1e51b47d1
Mathieu Lecarme <mathieu@garambrogne.net>
2023-07-23 17:27:08 +0200

    fix: criteria. fix: black. feat: purged.

--- purge.py
+++ purge.py
@@ -6,32 +6,41 @@ from datetime import datetime, timedelta
 from imapclient import IMAPClient
 import yaml
 
+HEADERS = set(["list-id", "list-archive"])
 
-assert os.getenv('IMAP'), "You need to set some ENVs"
 
-server = IMAPClient(os.getenv('IMAP'), use_uid=True, ssl=True)
-print(server.login(os.getenv('LOGIN'), os.getenv('PASSWORD')))
+assert os.getenv("IMAP"), "You need to set some ENVs"
+
+server = IMAPClient(os.getenv("IMAP"), use_uid=True, ssl=True)
+print(server.login(os.getenv("LOGIN"), os.getenv("PASSWORD")))
 
 
 rules = yaml.load(open("purge.yml", "r"), Loader=yaml.Loader)
 pprint(rules)
 delta = timedelta(days=rules["old"])
 
-select_info = server.select_folder('INBOX')
-print('%d messages in INBOX' % select_info[b'EXISTS'])
+select_info = server.select_folder("INBOX")
+print("%d messages in INBOX" % select_info[b"EXISTS"])
 
 now = datetime.now()
+purged = 0
 
-for purge in rules['purge']:
+for purge in rules["purge"]:
     if purge is None:
         continue
     k, v = list(purge.items())[0]
-    messages = server.search([k.upper(), v])
-    print("search", [k, v], len(messages))
+    criteria = [k.upper(), v]
+    if k.lower() in HEADERS:
+        criteria = ["HEADER", k, v]
+    messages = server.search(criteria)
+    print("search", criteria, len(messages))
     prunes = []
-    for msgid, data in server.fetch(messages, ['ENVELOPE']).items():
-        envelope = data[b'ENVELOPE']
+    for msgid, data in server.fetch(messages, ["ENVELOPE"]).items():
+        envelope = data[b"ENVELOPE"]
         if (now - envelope.date) > delta:
             prunes.append(msgid)
     print("Purge", len(prunes), "messages")
+    purged += len(prunes)
     server.delete_messages(prunes)
+
+print(purged, "purged")
