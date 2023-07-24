8eff39e73ecc1a1b5576688bcf08dcdd76ac5ccd
Mathieu Lecarme <mathieu@garambrogne.net>
2023-07-24 11:34:48 +0200

    feat: It's a lib

--- purge.py
+++ purge.py
@@ -1,46 +1,63 @@
 #!/usr/bin/env python3
 import os
 from pprint import pprint
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, date
 
 from imapclient import IMAPClient
 import yaml
 
-HEADERS = set(["list-id", "list-archive"])
+HEADERS = ["list", "x-github"]
 
 
-assert os.getenv("IMAP"), "You need to set some ENVs"
+def iterate_per_year(server: IMAPClient, start: int = 2000, stop: int = 2023):
+    for a in range(start, stop):
+        yield server.search(["BEFORE", date(a, 1, 1), "SINCE", date(a - 1, 1, 1)])
 
-server = IMAPClient(os.getenv("IMAP"), use_uid=True, ssl=True)
-print(server.login(os.getenv("LOGIN"), os.getenv("PASSWORD")))
+
+def flamer(
+    server: IMAPClient, rules: dict, box: str = "INBOX", debug: bool = False
+) -> int:
+    delta = timedelta(days=rules["old"])
+
+    select_info = server.select_folder(box)
+    if debug:
+        print("%d messages in INBOX" % select_info[b"EXISTS"])
+
+    now = datetime.now()
+    purged = 0
+
+    for purge in rules["purge"]:
+        if purge is None:
+            continue
+        k, v = list(purge.items())[0]
+        criteria = [k.upper(), v]
+        for prefix in HEADERS:
+            if k.lower().startswith(prefix):
+                criteria = ["HEADER", k, v]
+                break
+        messages = server.search(criteria)
+        if debug:
+            print("search", criteria, len(messages))
+        prunes = []
+        for msgid, data in server.fetch(messages, ["ENVELOPE"]).items():
+            envelope = data[b"ENVELOPE"]
+            if (now - envelope.date) > delta:
+                prunes.append(msgid)
+        if debug:
+            print("Purge", len(prunes), "messages")
+        purged += len(prunes)
+        server.delete_messages(prunes)
+    return purged
 
 
-rules = yaml.load(open("purge.yml", "r"), Loader=yaml.Loader)
-pprint(rules)
-delta = timedelta(days=rules["old"])
+if __name__ == "__main__":
+    assert os.getenv("IMAP"), "You need to set some ENVs"
 
-select_info = server.select_folder("INBOX")
-print("%d messages in INBOX" % select_info[b"EXISTS"])
+    server = IMAPClient(os.getenv("IMAP"), use_uid=True, ssl=True)
+    print(server.login(os.getenv("LOGIN"), os.getenv("PASSWORD")))
 
-now = datetime.now()
-purged = 0
+    rules = yaml.load(open("purge.yml", "r"), Loader=yaml.Loader)
+    pprint(rules)
 
-for purge in rules["purge"]:
-    if purge is None:
-        continue
-    k, v = list(purge.items())[0]
-    criteria = [k.upper(), v]
-    if k.lower() in HEADERS:
-        criteria = ["HEADER", k, v]
-    messages = server.search(criteria)
-    print("search", criteria, len(messages))
-    prunes = []
-    for msgid, data in server.fetch(messages, ["ENVELOPE"]).items():
-        envelope = data[b"ENVELOPE"]
-        if (now - envelope.date) > delta:
-            prunes.append(msgid)
-    print("Purge", len(prunes), "messages")
-    purged += len(prunes)
-    server.delete_messages(prunes)
-
-print(purged, "purged")
+    purged = flamer(server, rules, debug=True)
+    print(purged, "purged")
